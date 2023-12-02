cd6e53a48e6008fc2b93bc02d63db301ee6660cc
Anton Solbjørg <4324290+Notnaton@users.noreply.github.com>
2023-12-02 14:11:27 +0100

    Clean up model.py add req.txt

--- model.py
+++ model.py
@@ -1,35 +1,4 @@
 import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch_directml
-
-class Model(nn.Module):
-    def __init__(self):
-        self.device = torch_directml.device()
-
-    def load_tensor(self, file):
-        pass
-
-    def generate(self):
-        pass
-
-# Example: Read 4-bit quantized data (assuming it's in a binary file)
-with open('data.bin', 'rb') as f:
-    data = f.read()
-
-# Convert bytes to 8-bit integers
-data_8bit = np.frombuffer(data, dtype=np.uint8)
-
-# Split each 8-bit integer into two 4-bit values
-data_4bit = np.vstack(((data_8bit & 0xF0) >> 4, data_8bit & 0x0F)).reshape(-1, order='F')
-
-# (Optional) Apply scale and zero-point
-# scale = ...
-# zero_point = ...
-# data_float = (data_4bit.astype(np.float32) - zero_point) * scale
-
-# Create a PyTorch tensor
-tensor = torch.tensor(data_4bit, dtype=torch.uint8)  # or torch.float32 if converted
-
-# Further processing...
+from tinygrad.tensor import Tensor
+from tinygrad.helpers import dtypes
+from tinygrad.nn import Linear, Embedding
