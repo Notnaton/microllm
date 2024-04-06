f748246b0838fef9aeb6dd3b2cc94a4ef08dbfd7
Anton Solbjørg <4324290+Notnaton@users.noreply.github.com>
2024-04-06 15:15:52 +0200

    Moving stuff trying to fix poetry

--- model/main.py
+++ model/main.py
@@ -5,5 +5,6 @@ from tinygrad.nn import Linear, Embedding
 from typing import List
 from file_type import gguf
 
-data, file = gguf.read_gguf("E:\LLM\models\TheBloke\Mistral-7B-Instruct-v0.1-GGUF\mistral-7b-instruct-v0.1.Q4_0.gguf")
-print(file)
+def main():
+    data, file = gguf.read_gguf("E:\LLM\models\TheBloke\Mistral-7B-Instruct-v0.1-GGUF\mistral-7b-instruct-v0.1.Q4_0.gguf")
+    print(file)
